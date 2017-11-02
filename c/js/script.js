72e2f2244dbccf05466b550a1ea8ec8afd6dc174
CRCHAN <670187107@qq.com>
2017-11-02 23:29:17 +0800

    add the game's frame -- GAME

--- c/js/script.js
+++ c/js/script.js
@@ -1,7 +1,16 @@
-window.onload = function(){
-	
-}
-var oStart = document.querySelector(".js-start"),
+var oBody = document.body;
+var canvas = document.getElementById("game");
+var ctx = canvas.getContext("2d");
+//设置画布的宽和高
+canvas.width = window.innerWidth; //IE8及更早的版本不支持innerWidth与innerHeight属性
+canvas.height = window.innerHeight;
+//获取画布信息
+var canvasWidth = canvas.clientWidth;
+var canvasHeight = canvas.clientHeight;
+
+
+function bindEvent(){
+	var oStart = document.querySelector(".js-start"),
 		oSetting = document.querySelector(".js-setting"),
 		oRule = document.querySelector(".js-rule"),
 		oUiIndex = document.querySelector(".ui-index"),
@@ -13,6 +22,7 @@ var oStart = document.querySelector(".js-start"),
 		oUiIndex.style.display = "none";
 		oUiRule.style.display = "none";
 		oUiSetting.style.display = "none";
+		GAME.start();
 	}
 	oRule.onclick = function(){
 		oUiIndex.style.display = "none";
@@ -23,7 +33,6 @@ var oStart = document.querySelector(".js-start"),
 		oUiIndex.style.display = "none";
 		oUiRule.style.display = "none";
 		oUiSetting.style.display = "block";
-		console.log("?");
 	}
 	oRuleKnow.onclick = function(){
 		oUiIndex.style.display = "block";
@@ -34,4 +43,40 @@ var oStart = document.querySelector(".js-start"),
 		oUiIndex.style.display = "block";
 		oUiRule.style.display = "none";
 		oUiSetting.style.display = "none";
+	}	
+}
+
+	
+/* 游戏对象 */
+var GAME = {
+	/* 游戏初始化 */
+	init: function(opts){
+		//设置opts
+		var opts = Object.assign({}, opts, CONFIG); //合并对象
+		this.opts = opts;
+		
+		//计算飞机初始坐标
+		this.planePosX = canvasWidth / 2 - opts.planeSize.width / 2;
+		this.planePosY = canvasHeight - opts.planeSize.height - 50;
+	},
+	start: function(){
+		
+	},
+	update: function(){
+		
+	},
+	end: function(){
+		
+	},
+	draw: function(){
+		
 	}
+};
+
+function init(){
+	GAME.init();
+	bindEvent();
+}
+
+
+init();
